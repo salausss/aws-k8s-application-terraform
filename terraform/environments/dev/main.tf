6f596bbcd9e4d88739635d2b4ff16938a92bccd9
salausss <salauss00@gmail.com>
2026-03-26 22:49:04 +0530

    update region

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   project_name          = var.project_name
   env                   = var.environment
   vpc_cidr            = "10.1.0.0/16"
-  azs                 = ["us-east-1a", "us-east-1b"]
+  azs                 = ["ap-south-1a", "ap-south-1b"]
   public_subnet_cidrs  = ["10.1.1.0/24", "10.1.2.0/24"]
   private_subnet_cidrs = ["10.1.11.0/24", "10.1.12.0/24"]
 }
