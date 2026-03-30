5ef86dbdee03ed3f48ea990a34ea11900f575eeb
salausss <salauss00@gmail.com>
2026-03-30 21:31:41 +0530

    update configmap

--- terraform/environments/dev/providers.tf
+++ terraform/environments/dev/providers.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+  cluster_ca_certificate = module.eks.cluster_ca_certificate
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
