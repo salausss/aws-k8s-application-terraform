0aafeb8ff0ccf3963232f4b97f7986ab68bfb5de
salausss <salauss00@gmail.com>
2026-04-02 17:02:39 +0530

    update path

--- terraform/modules/k8s/main.tf
+++ terraform/modules/k8s/main.tf
@@ -1,12 +1,12 @@
 resource "helm_release" "taskflow" {
   name       = "taskflow"
-  chart      = "${path.root}/../../k8s"
+  chart      = "${path.root}/../../src/k8s"
   namespace  = "app"
 
   create_namespace = false
 
   values = [
-    file("${path.root}/../../k8s/values/dev.yaml")
+    file("${path.root}/../../src/k8s/values/dev.yaml")
   ]
 
   # 🔥 Important for stability
