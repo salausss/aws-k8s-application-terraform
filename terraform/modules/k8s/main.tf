cdb35637708ebfc96b7f7f55b6989afacc70caed
salausss <salauss00@gmail.com>
2026-04-02 16:43:20 +0530

    update local config

--- terraform/modules/k8s/main.tf
+++ terraform/modules/k8s/main.tf
@@ -1,8 +1,18 @@
 locals {
-  manifests = {
-    for f in fileset("${path.root}/../../src/k8s/", "*.yaml") :
-    f => yamldecode(file("${path.root}/../../src/k8s/${f}"))
-  }
+  k8s_path = "${path.module}/../../src/k8s"
+
+  manifests = flatten([
+    for f in fileset(local.k8s_path, "*.yaml") : [
+      for idx, doc in split("---", file("${local.k8s_path}/${f}")) : {
+        key = "${f}-${idx}"
+        content = try(
+          yamldecode(trimspace(doc)),
+          error("❌ YAML ERROR in file: ${f} (doc index: ${idx})")
+        )
+      }
+      if trimspace(doc) != ""
+    ]
+  ])
 }
 
 resource "kubernetes_manifest" "this" {
