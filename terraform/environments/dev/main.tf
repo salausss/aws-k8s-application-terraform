b8e78004267490d1a43da30882e5916c45c0ca22
salausss <salauss00@gmail.com>
2026-04-13 13:02:21 +0530

    update sg

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -67,7 +67,7 @@ module "efs" {
   environment            = var.environment
   vpc_id                 = module.vpc.vpc_id
   private_subnet_ids     = module.vpc.private_subnet_ids
-  node_security_group_id = module.eks.node_security_group_id
+  node_security_group_id = module.eks.cluster_security_group_id
 }
 
 #module "k8s" {
