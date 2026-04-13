ca78d19c1e130741cfdbc5e4af883e312e863053
salausss <salauss00@gmail.com>
2026-04-14 02:28:23 +0530

    update config

--- terraform/modules/eks/output.tf
+++ terraform/modules/eks/output.tf
@@ -25,7 +25,7 @@ output "oidc_provider_url" {
 
 output "oidc_provider_arn" {
   description = "OIDC provider ARN for IRSA"
-  value       = "aws_iam_openid_connect_provider.this.arn"
+  value       = aws_iam_openid_connect_provider.this.arn
 }
 
 output "configure_kubectl" {
